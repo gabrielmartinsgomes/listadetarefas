945d0e6a18324e8357e92d3ac36ff09a4808557d
gabrielmartinsgomes <gabrielhenrickmartind3454@gmail.com>
2023-12-06 16:38:00 -0300

    ;

--- script.js
+++ script.js
@@ -2,32 +2,32 @@ let adicionar = document.getElementById("adicionar");
 let afazer = document.getElementById("afazer");
 let texto = document.getElementById("texto")
 let botaofinish = document.getElementById("finish");
-let botaofinish2 = document.getElementById("finish2")
-let tarefacompleta = document.getElementById("tarefacompleta")
+let botaofinish2 = document.getElementById("finish2");
+let tarefacompleta = document.getElementById("tarefacompleta");
 
 adicionar.addEventListener("click", function(){
     let paragrafo = document.createElement("p");
     paragrafo.classList.add('paragrafostyle');
     paragrafo.innerText = texto.value; 
     afazer.appendChild(paragrafo);
-    texto.value = ""
-    texto.focus()
+    texto.value = "";
+    texto.focus();
     paragrafo.addEventListener("click", function(){
         afazer.removeChild(paragrafo);
         tarefacompleta.appendChild(paragrafo);
-        texto.value = ""
-        texto.focus()
+        texto.value = "";
+        texto.focus();
     })
     botaofinish.addEventListener("click", function(){
         afazer.removeChild(paragrafo);
         tarefacompleta.appendChild(paragrafo);
-        texto.value = ""
-        texto.focus()
+        texto.value = "";
+        texto.focus();
     })
     botaofinish2.addEventListener("click", function(){
         tarefacompleta.removeChild(paragrafo);
-        texto.value = ""
-        texto.focus()
+        texto.value = "";
+        texto.focus();
     })
     
 })
